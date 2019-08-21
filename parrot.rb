6f4262cc8e3a6defea432ed745434a6e4d75d349
Eli Kim <eliskim27@gmail.com>
2019-08-21 17:02:17 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string)
-  string
-  if TRUE
+  if string == TRUE
     puts "#{string}"
    string
   else
     puts "Squawk!"
     string = "Squawk!"
   end
+  string
 end
